4b093ad5db838cf325e507105641d55a3ad175ee
feiyu <1348453525@qq.com>
2026-04-20 18:32:06 +0800

    更新自定义错误 err：错误码+描述；更新 response：解析 rpc 返回的自定义错误码；

--- pkg/errorx/err.go
+++ pkg/errorx/err.go
@@ -8,34 +8,48 @@ import (
 )
 
 func ToGrpcError(err error) error {
-	if errors.Is(err, ErrInternal) {
-		return status.Error(codes.Internal, err.Error())
-	} else if errors.Is(err, ErrParam) {
-		return status.Error(codes.InvalidArgument, err.Error())
+	// if errors.Is(err, ErrInternal) {
+	// 	return status.Error(codes.Internal, err.Error())
+	// } else if errors.Is(err, ErrParam) {
+	// 	return status.Error(codes.InvalidArgument, err.Error())
+	// }
+	// return status.Error(codes.Unknown, err.Error())
+
+	// switch {
+	// case errors.Is(err, ErrInternal):
+	// 	return status.Error(codes.Code(ErrInternal.Code), ErrInternal.Message)
+	// case errors.Is(err, ErrParam):
+	// 	return status.Error(codes.Code(ErrParam.Code), ErrParam.Message)
+	// default:
+	// 	return status.Error(codes.Unknown, err.Error())
+	// }
+
+	var e *Errorx
+	if errors.As(err, &e) {
+		return status.Error(codes.Code(e.Code), e.Message)
 	}
 	return status.Error(codes.Unknown, err.Error())
 }
 
 // 系统错误
 var (
-	ErrInternal = errors.New("内部错误")
-
-	ErrParam           = errors.New("参数错误")
-	ErrOperationFailed = errors.New("操作失败")
+	ErrInternal        = New(500, "内部错误")
+	ErrParam           = New(400, "参数错误")
+	ErrOperationFailed = New(500, "操作失败")
 )
 
 // 用户错误
 var (
-	ErrPasswordNotMatch = errors.New("密码不匹配")
-	ErrUserExisted      = errors.New("用户已存在")
-	ErrUserDisabled     = errors.New("账户已停用")
-	ErrPasswordError    = errors.New("密码错误")
-	ErrUserNotExisted   = errors.New("用户不存在")
+	ErrPasswordNotMatch = New(1401, "密码不匹配")
+	ErrUserExisted      = New(1402, "用户已存在")
+	ErrUserDisabled     = New(1403, "账户已停用")
+	ErrPasswordError    = New(1404, "密码错误")
+	ErrUserNotExisted   = New(1405, "用户不存在")
 )
 
 // 兑换码错误
 var (
-	ErrRedeemCodeUsedUp  = errors.New("兑换码已用完")
-	ErrRedeemCodeExpired = errors.New("兑换码已过期")
-	ErrRedeemCodeInvalid = errors.New("兑换码无效")
+	ErrRedeemCodeUsedUp  = New(1401, "兑换码已用完")
+	ErrRedeemCodeExpired = New(1402, "兑换码已过期")
+	ErrRedeemCodeInvalid = New(1403, "兑换码无效")
 )
--- pkg/response/response.go
+++ pkg/response/response.go
@@ -5,6 +5,8 @@ import (
 
 	"github.com/1348453525/user-redeem-code-gozero/pkg/errorx"
 	"github.com/zeromicro/go-zero/rest/httpx"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 // 统一响应体结构体
@@ -43,11 +45,51 @@ func Errorx(w http.ResponseWriter, err error) {
 		response.Code = e.Code
 		response.Message = e.Message
 	default:
-		response.Code = 500
-		response.Message = e.Error()
+		if grpcStatus, ok := status.FromError(e); ok {
+			response.Code = grpcCodeToHttpCode(grpcStatus.Code())
+			response.Message = grpcStatus.Message()
+		} else {
+			response.Code = 500
+			response.Message = e.Error()
+		}
 	}
 
 	response.Data = struct{}{}
 
 	httpx.OkJson(w, response)
+}
+
+func grpcCodeToHttpCode(code codes.Code) int {
+	switch code {
+	case codes.OK:
+		return 200 // http.StatusOK
+	case codes.InvalidArgument, codes.FailedPrecondition, codes.OutOfRange:
+		return 400 // http.StatusBadRequest
+	case codes.Unauthenticated:
+		return 401 // http.StatusUnauthorized
+	case codes.NotFound:
+		return 404 // http.StatusNotFound
+	case codes.AlreadyExists, codes.Aborted:
+		return 409 // http.StatusConflict
+	case codes.PermissionDenied:
+		return 403 // http.StatusForbidden
+	case codes.ResourceExhausted:
+		return 429 // http.StatusTooManyRequests
+	case codes.Canceled:
+		return 499
+	case codes.Unknown, codes.Internal, codes.DataLoss:
+		return 500 // http.StatusInternalServerError
+	case codes.Unimplemented:
+		return 501 // http.StatusNotImplemented
+	case codes.Unavailable:
+		return 503 // http.StatusServiceUnavailable
+	case codes.DeadlineExceeded:
+		return 504 // http.StatusGatewayTimeout
+	default:
+		// 判断 code 是 err.go 中自定义的错误码，返回自定的错误码
+		if code >= 1000 {
+			return int(code)
+		}
+		return 500 // http.StatusInternalServerError
+	}
 }
