cbf2ef425d68b8f06d07b3353a289abb8c53685e
feiyu <1348453525@qq.com>
2026-04-20 13:57:55 +0800

    api：增加 rpc 配置

--- api/internal/svc/servicecontext.go
+++ api/internal/svc/servicecontext.go
@@ -5,14 +5,22 @@ package svc
 
 import (
 	"github.com/1348453525/user-redeem-code-gozero/api/internal/config"
+	"github.com/1348453525/user-redeem-code-gozero/redeem-code-rpc/redeemcodeclient"
+	userclient "github.com/1348453525/user-redeem-code-gozero/user-rpc/client/user"
+	"github.com/zeromicro/go-zero/zrpc"
+	_ "github.com/zeromicro/zero-contrib/zrpc/registry/consul"
 )
 
 type ServiceContext struct {
-	Config config.Config
+	Config        config.Config
+	UserRpc       userclient.User
+	RedeemCodeRpc redeemcodeclient.RedeemCode
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
 	return &ServiceContext{
-		Config: c,
+		Config:        c,
+		UserRpc:       userclient.NewUser(zrpc.MustNewClient(c.UserRpc)),
+		RedeemCodeRpc: redeemcodeclient.NewRedeemCode(zrpc.MustNewClient(c.RedeemCodeRpc)),
 	}
 }
